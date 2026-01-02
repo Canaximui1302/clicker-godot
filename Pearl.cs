e923a6ca40dbd014db01712578b33ea1f27c906f
Minh Khanh Nguyen <minhkhanh13022004@gmail.com>
2026-01-02 14:09:51 +0100

    added random position on click

--- Pearl.cs
+++ Pearl.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
+
 public partial class Pearl : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
@@ -13,12 +14,19 @@ public partial class Pearl : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Input(InputEvent @ev)
 	{
+		var ScreenSize = GetViewportRect();
+		int distance = 100;
+		int X = (int) ScreenSize.Size.X;
+		int Y = (int) ScreenSize.Size.Y;
+		Random rnd = new Random();
 		if (@ev is InputEventMouseButton mouseEv && mouseEv.Pressed)
 		{
 			if (mouseEv.ButtonIndex == MouseButton.Left)
 			{
 				GD.Print("Left clicked");
-				this.Position += new Vector2(2, 0);
+				var randX = rnd.Next(distance, X - distance + 1);
+				var randY = rnd.Next(distance, Y - distance + 1);
+				this.Position = new Vector2(randX, randY);
 			}
 		}
 	}
