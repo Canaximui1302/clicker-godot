2b945d8d75c76c828906ff5d1e300584fb9a9430
Minh Khanh Nguyen <minhkhanh13022004@gmail.com>
2026-01-03 00:28:17 +0100

    detach buttons scripts

--- Menu.cs
+++ Menu.cs
@@ -4,12 +4,9 @@ using System;
 public partial class Menu : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	private void _Quit()
 	{
+		GetTree().Quit();
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	
 }
