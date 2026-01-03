d3cb8a53b09642976176b0e8305b6703393573e6
Minh Khanh Nguyen <minhkhanh13022004@gmail.com>
2026-01-03 01:52:37 +0100

    Settings button worked

--- Menu.cs
+++ Menu.cs
@@ -1,8 +1,30 @@
 using Godot;
 using System;
 
-public partial class Menu : Node2D
+public partial class Menu : Control
 {
+	private Control mainMenu;
+    private Control optionsMenu;
+
+    public override void _Ready()
+    {
+        mainMenu = GetNode<Control>("Main");
+        optionsMenu = GetNode<Control>("Settings");
+
+		ShowMain();
+    }
+
+	private void ShowMain()
+	{
+		mainMenu.Show();
+		optionsMenu.Hide();
+	}
+
+	private void ShowSettings()
+	{
+		mainMenu.Hide();
+		optionsMenu.Show();
+	}
 	// Called when the node enters the scene tree for the first time.
 	private void _Quit()
 	{
@@ -12,6 +34,16 @@ public partial class Menu : Node2D
 	private void _Start()
 	{
 		GetTree().ChangeSceneToFile("res://main_scene.tscn");
+	}
+
+	private void _Settings()
+	{
+		ShowSettings();
+	}
+
+	private void _Back()
+	{
+		ShowMain();
 	}
 	
 }
