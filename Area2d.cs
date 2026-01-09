622ebc77cd558e4796982f051e4bbbb4d6b6b179
Minh Khanh Nguyen <minhkhanh13022004@gmail.com>
2026-01-09 23:11:23 +0100

    adding more cmt

--- Area2d.cs
+++ Area2d.cs
@@ -40,13 +40,15 @@ public partial class Area2d : Area2D
 		Score.Text = "Score: " + GameState.Lastscore.ToString();
     }
 
-
+	// Use InputEvent to take mouse left click input
+	// InputEvent records input that happens on the node
+	// In this case we want to detect clicks on the character
+	// and generate new position for the character after each click, and increase the score as well
 	public override void _InputEvent(Viewport viewport, InputEvent @ev, int shapeIdx)
 	{
 		if (@ev is InputEventMouseButton mouse && mouse.ButtonIndex == MouseButton.Left && mouse.Pressed)
         {
-            // GD.Print("Character clicked!");
-
+            // GD.Print("Character clicked!"); // for debugging
             Vector2 screenSize = viewport.GetVisibleRect().Size;
 
             float x = rng.RandfRange(Margin, screenSize.X - Margin);
@@ -54,12 +56,14 @@ public partial class Area2d : Area2D
 			GameState.Lastscore += 1;
             Position = new Vector2(x, y);
         }
-		if (@ev is InputEventKey key && key.Pressed && key.Keycode == Key.Escape)
-		{
-			pauseMenu.Show();
-		}
+
+		// If Esc key is pressed, show up the paused menu
 	}
 
+
+	// If Esc key is pressed, show up the paused menu\
+	// Here we use Input because the key press event can be 
+	// detected globally on the scene, not on any specific node
 	public override void _Input(InputEvent @key)
 	{
 		if (@key is InputEventKey k && k.Pressed && k.Keycode == Key.Escape)
@@ -70,6 +74,8 @@ public partial class Area2d : Area2D
 		}
 	}
 
+	// When timer is done counting down, change to the main menu scene 
+	// and save the score of the last game
 	public  void _Timeout()
 	{
 		GameState.Save_score();
